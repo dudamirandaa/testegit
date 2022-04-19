9dfaf1fdd0fdd7e9987d1922d51d952b517b0c2f
dudamirandaa <eduardamiranda1901@icloud.com>
2022-04-19 10:46:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res)=>{
     res.status(200).send({ message: 'Olá mundo estou funcionando!' })
 })
 
+app.get('/ola', (req, res)=>{
+    res.status(200).send({ message: 'Eu sou uma ota Olá!' })
+})
+
 app.listen(3001, () => {
     console.log('API rodando na porta 3001');
 })
